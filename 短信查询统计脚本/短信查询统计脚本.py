b67a0e6d6c36f7815198ba4194d824c704b19547
ohuihui <13868139011@139.com>
2017-12-27 15:23:28 +0800

    20171227 15:23

--- 短信查询统计脚本/短信查询统计脚本.py
+++ 短信查询统计脚本/短信查询统计脚本.py
@@ -1,27 +1,41 @@
 import pypyodbc
 SQLConnctString = r'DRIVER={SQL Server};SERVER=10.212.172.226;DATABASE=UserDataQueryProject;UID=sa;PWD=jiaohuan@288'
-CommandLine = r'''SELECT Userinfo.NAME, COUNT(1) AS count
+Cmd_eachUser = r'''SELECT Userinfo.NAME, COUNT(1) AS count
                 FROM cmpp.dbo.CmSTATINFO INNER JOIN
                     Userinfo ON cmpp.dbo.CmSTATINFO.mobile = Userinfo.Num
                 WHERE (cmpp.dbo.CmSTATINFO.smsport = '106585841') AND (DATEDIFF(DAY,
                     cmpp.dbo.CmSTATINFO.sendtime, GETDATE()) <= 7)
                 GROUP BY Userinfo.NAME
                 ORDER BY count DESC'''
+Cmd_count = r'''SELECT count(1) as a
+                FROM [cmpp].[dbo].[CmSTATINFO]
+                where smsport = '106585841' and (DATEDIFF(DAY,
+                                    cmpp.dbo.CmSTATINFO.sendtime, GETDATE()) <= 7)
+                group by smsport'''
+
+
 sendSMSCommandPatten = r'''insert into [cmpp].[dbo].CmMETONEMTINFO (mobile,smscontent,smsport,isinqueue,userorder,sendtime,registered)
                     values ('{num}','{content}','1065858499',0,0,getdate(),1);'''
-listDesNum = [13868139011]  # 放入需要发送短信的联系人
+listDesNum = ['13868139011' #, '13858115116', '13806510651', '13777456086', '13958053500'
+              ]  # 放入需要发送短信的联系人
+
 threshold = 20
-
-
 
 
 def __main():
     cnxn = pypyodbc.connect(SQLConnctString, unicode_results=True)
     cur = cnxn.cursor()
-    sendConfirm=False
+    sendConfirm = False
     try:
-        cur.execute(CommandLine)
-        str_result = '本周短信接口用户查询次数：\r\n用户  查询次数\r\n'
+        str_result = '本周华为短信接口用户查询次数统计：\r\n'
+        row = cur.execute(Cmd_count).fetchall()
+        count = row[0][0]
+        str_result += '本周共完成查询' + \
+            str(count) + '次\r\n本周查询大于' + \
+            str(threshold) + '次用户：\r\n用户  查询次数\r\n'
+
+        cur.execute(Cmd_eachUser)
+
         for row in cur.fetchall():
             if row[1] >= threshold:
                 str_result = str_result + \
@@ -31,10 +45,10 @@ def __main():
         for desNum in listDesNum:
             if sendConfirm == True:
                 sendsmsCommand = sendSMSCommandPatten.format(
-                    num='13868139011', content=str_result)
+                    num=desNum, content=str_result)
             else:
                 sendsmsCommand = sendSMSCommandPatten.format(
-                    num='13868139011', content='本周短信接口用户查询次数：\r\n本周无查询' + str(threshold) + '次及以上用户')
+                    num='13868139011', content='本周华为短信接口用户查询次数统计：\r\n本周共完成查询' + str(count) + '次\r\n本周无查询大于' + str(threshold) + '次及以上用户')
             cur.execute(sendsmsCommand)
             cur.commit()
 
