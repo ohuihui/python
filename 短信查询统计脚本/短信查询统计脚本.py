393621dc43225ffe374b6b180442a290a092be7b
ohuihui <13868139011@139.com>
2017-12-27 14:06:32 +0800

    20171227-2

--- 短信查询统计脚本/短信查询统计脚本.py
+++ 短信查询统计脚本/短信查询统计脚本.py
@@ -7,22 +7,42 @@ CommandLine = r'''SELECT Userinfo.NAME, COUNT(1) AS count
                     cmpp.dbo.CmSTATINFO.sendtime, GETDATE()) <= 7)
                 GROUP BY Userinfo.NAME
                 ORDER BY count DESC'''
+sendSMSCommandPatten = r'''insert into [cmpp].[dbo].CmMETONEMTINFO (mobile,smscontent,smsport,isinqueue,userorder,sendtime,registered)
+                    values ('{num}','{content}','1065858499',0,0,getdate(),1);'''
+listDesNum = [13868139011]  # 放入需要发送短信的联系人
+threshold = 20
+
+
 
 
 def __main():
     cnxn = pypyodbc.connect(SQLConnctString, unicode_results=True)
     cur = cnxn.cursor()
+    sendConfirm=False
     try:
         cur.execute(CommandLine)
+        str_result = '本周短信接口用户查询次数：\r\n用户  查询次数\r\n'
         for row in cur.fetchall():
-            print(row)
+            if row[1] >= threshold:
+                str_result = str_result + \
+                    row[0].strip() + '  ' + str(row[1]) + '\r\n'
+                sendConfirm = True
+
+        for desNum in listDesNum:
+            if sendConfirm == True:
+                sendsmsCommand = sendSMSCommandPatten.format(
+                    num='13868139011', content=str_result)
+            else:
+                sendsmsCommand = sendSMSCommandPatten.format(
+                    num='13868139011', content='本周短信接口用户查询次数：\r\n本周无查询' + str(threshold) + '次及以上用户')
+            cur.execute(sendsmsCommand)
+            cur.commit()
 
     except Exception as e:
         print(e)
 
     finally:
         cnxn.close()
-
 
 
 if __name__ == '__main__':
